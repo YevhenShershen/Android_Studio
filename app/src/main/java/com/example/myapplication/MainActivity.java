becc7ec705c42a3ebd9bf64ced1970c2a493be81
Yevhen <shershen.yevhen@gmai>
2022-12-22 17:22:36 +0100

    Увеличение при нажатии кнопки

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,37 +2,28 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView imageView2;
-    private EditText photo_show;
-    private Button button_next;
-    private Button button_prev;
-    private Switch switch_fon;
+
+    private TextView textView;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        count = 0;
+        textView = findViewById(R.id.textView);
+        Button button123 = findViewById(R.id.button123);
 
-        photo_show = findViewById(R.id.photo_show);
-        button_next = findViewById(R.id.button_next);
-        button_next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 
-    public void clickNext(View view) {
-        imageView2 = findViewById(R.id.imageView2);
+    public void nextCount(View view) {
+        count++;
+        textView.setText(String.valueOf(count + ""));
     }
 }
